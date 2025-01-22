b07f964faff0ddd2148054120c86097f260c58b0
manish2588 <manishkc258@gmail.com>
2025-01-22 22:00:04 +0545

    Added

--- tsc/array.ts
+++ tsc/array.ts
@@ -1,3 +1,10 @@
 
 const arr:number[]=[1,2,3];
+const arr1:number[]=[1,2,3];
 console.log(arr)
+
+console.log(arr)
+const arr2:number[]=[1,2,3,44,5,6,7,88,9,15]
+
+const newAry:number=arr2.reduce((acc,value)=>Math.max(acc,value),0)
+console.log(newAry)
